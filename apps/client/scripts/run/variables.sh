b3074186374d59df24284bc52eca358ecc26b786
Cole <cole@stowell.pro>
2023-12-01 16:47:53 -0500

    Fix changing static files on RUN

--- apps/client/scripts/run/variables.sh
+++ apps/client/scripts/run/variables.sh
@@ -9,20 +9,6 @@ else
     VAR_PATH="/app/apps/client/build"
 fi
 
-cp "$VAR_PATH/variables-template.js" "$VAR_PATH/variables.js"
-
-if [[ -z "$API_ENDPOINT" ]]
-then
-    echo "API_ENDPOINT is not defined, web app won't work"
-    exit 1
-fi
-
-echo "Setting API Endpoint to '$API_ENDPOINT'"
-sed -i "s;__API_ENDPOINT__;$API_ENDPOINT;g" "$VAR_PATH/variables.js"
-
-# Editing meta image urls
-sed -i "s;image\" content=\"\(.[^\"]*\);image\" content=\"$API_ENDPOINT/static/your_spotify_1200.png;g" "$VAR_PATH/index.html"
-
 # Restricting connect-src to API_ENDPOINT with a trailing /, or to * if hostname has an _
 CSP_CONNECT_SRC=$API_ENDPOINT
 if [[ "$CSP_CONNECT_SRC" == *_*.*.* ]]
